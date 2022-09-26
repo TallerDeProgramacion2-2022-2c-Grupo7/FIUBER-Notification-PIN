86ec49915d4d064d382cb48781ff369522553e4d
Martin Stefanelli <martin.stefanelli.96@hotmail.com>
2022-09-25 21:32:25 -0300

    Subo la app y config. de github actions

--- fastapi_app/app.py
+++ fastapi_app/app.py
@@ -1,4 +1,5 @@
 import asyncio
+import uvicorn
 from email import message
 from random import randint
 from fastapi import FastAPI, Form, status
@@ -10,26 +11,26 @@ import config
 app = FastAPI()
 settings = config.Settings()
 
-class UserNumber(BaseModel):
-    _user_number: str
-
 class PinNumber(BaseModel):
-    _pin: str
+    pin: str
+    mensaje: str | None = None
 
-pin = randint(1000000, 9999999)
-message_ = "Tu código de verificación es: {}".format(pin)
-#message = Message(_message=message_)
 
 app = FastAPI()
 
-@app.post("/users_number/", response_model=PinNumber)
-async def send_messege(user_number: UserNumber):
+@app.post("/users_number/{user_number}", response_model=PinNumber)
+async def send_messege(user_number: str):
+    pin_ = str(randint(1000000, 9999999))
+    message_ = "Tu código de verificación de FIUBER es: {}".format(pin_)
     await asyncio.get_event_loop().run_in_executor(
-        None, send_whatsapp, user_number._user_number, message_)
+        None, send_whatsapp, user_number, message_)
     
-    return PinNumber(_pin=pin)
+    return {"pin":pin_}
 
 def send_whatsapp(to_number, body):
     client = Client(settings.twilio_account_sid, settings.twilio_auth_token)
-    return client.messages.create(from_=settings.twilio_phone_number,
-                                  to=to_number, body=body)
+    return client.messages.create(from_="whatsapp:{}".format(settings.twilio_phone_number),
+                                  body=body, to="whatsapp:{}".format(to_number))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
