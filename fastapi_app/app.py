4043c74da0204e7ac21504bda64bb4f939f6ed52
Martin Stefanelli <martin.stefanelli.96@hotmail.com>
2022-10-10 22:50:53 -0300

    Subo el backend para enviar un msj por wpp

--- fastapi_app/app.py
+++ fastapi_app/app.py
@@ -1,36 +1,58 @@
 import asyncio
 import uvicorn
 from email import message
-from random import randint
-from fastapi import FastAPI, Form, status
+from fastapi import FastAPI, Form, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, RedirectResponse
 from twilio.rest import Client
 from pydantic import BaseModel
 import config
+import firebase_admin
+from firebase_admin import credentials
+from firebase_credentials import admin_credentials
+from id_token import IdTokenMiddleware
+
+firebase_credentials = credentials.Certificate(admin_credentials)
+firebase_admin.initialize_app(firebase_credentials)
+
+class Body(BaseModel):
+    to_number: str
+    verification_code: str
 
 app = FastAPI()
 settings = config.Settings()
 
-class PinNumber(BaseModel):
-    pin: str
-    mensaje: str | None = None
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+app.add_middleware(IdTokenMiddleware)
 
 
 app = FastAPI()
 
-@app.post("/users_number/{user_number}", response_model=PinNumber)
-async def send_messege(user_number: str):
-    pin_ = str(randint(1000000, 9999999))
-    message_ = "Tu código de verificación de FIUBER es: {}".format(pin_)
-    await asyncio.get_event_loop().run_in_executor(
-        None, send_whatsapp, user_number, message_)
+@app.post("/send_pin/")
+async def send_messege(body: Body):
+    message_ = "Tu código de verificación de FIUBER es: {}".format(body.verification_code)
+    future = await asyncio.get_event_loop().run_in_executor(
+        None, send_whatsapp, body.to_number, message_)
     
-    return {"pin":pin_}
+    return future.add_done_callback(callback)
 
 def send_whatsapp(to_number, body):
     client = Client(settings.twilio_account_sid, settings.twilio_auth_token)
     return client.messages.create(from_="whatsapp:{}".format(settings.twilio_phone_number),
                                   body=body, to="whatsapp:{}".format(to_number))
+
+def callback(future):
+    if future.exception():
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Error al enviar el mensaje")
+    
+    return future.result()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
